d327e84b3c86bb7ff216a691d19e9f313c825797
Thallysson Klein <thallyssonklein@gmail.com>
2021-07-11 08:16:34 +0000

    PaymentsAPI initial structure

--- PaymentsAPI/src/main/java/com/payments/PaymentsAPI/PaymentsApiApplication.java
+++ PaymentsAPI/src/main/java/com/payments/PaymentsAPI/PaymentsApiApplication.java
@@ -1,13 +1,23 @@
 package com.payments.PaymentsAPI;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "com.payments.PaymentsAPI")
+@EnableSwagger2
 @SpringBootApplication
 public class PaymentsApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentsApiApplication.class, args);
 	}
-
 }
