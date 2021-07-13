0b441a3dbdc95e4ab25a424d1eb65193678d4dc4
Thallysson Klein <thallyssonklein@gmail.com>
2021-07-13 19:39:02 +0000

    Test

--- PaymentsAPI/src/main/java/com/payments/PaymentsAPI/service/impl/ProducerServiceImpl.java
+++ PaymentsAPI/src/main/java/com/payments/PaymentsAPI/service/impl/ProducerServiceImpl.java
@@ -4,24 +4,34 @@ import com.google.gson.Gson;
 import com.payments.PaymentsAPI.entity.Payment;
 import com.payments.PaymentsAPI.service.ProducerService;
 
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
-import org.springframework.util.concurrent.ListenableFuture;
 
 @Service
 public final class ProducerServiceImpl implements ProducerService {
     
-    private final KafkaTemplate<String, String> kafkaTemplate;
-    private final String TOPIC = "kafkaTopic";
     private Gson gson = new Gson();
+    private KafkaProducer<String,String> producer = new KafkaProducer<String,String>(properties());
 
-    public ProducerServiceImpl(KafkaTemplate<String, String> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
     }
-
+    
     @Override
     public void sendMessage(Payment message) {
-        ListenableFuture<SendResult<String, String>> future = this.kafkaTemplate.send(TOPIC, gson.toJson(message));
+        var key = "TEMPERATURA";
+        var value = "34º";
+        var record = new ProducerRecord<String, String>("EXEMPLO_TOPICO", key, value);
+        Callback callback = (data, ex) -> {
+            if (ex != null) {
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println("Mensagem enviada com sucesso para: " + data.topic() + " | partition " + data.partition() + "| offset " + data.offset() + "| tempo " + data.timestamp());
+        };
+        producer.send(record, callback).get();
     }
 }
