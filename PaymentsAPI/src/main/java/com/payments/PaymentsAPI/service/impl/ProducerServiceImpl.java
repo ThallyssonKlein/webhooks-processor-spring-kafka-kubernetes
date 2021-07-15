0a039b4cee7864bd3d429441dc0d457e1504b646
Thallysson Klein <thallyssonklein@gmail.com>
2021-07-15 18:42:49 +0000

    Test4

--- PaymentsAPI/src/main/java/com/payments/PaymentsAPI/service/impl/ProducerServiceImpl.java
+++ PaymentsAPI/src/main/java/com/payments/PaymentsAPI/service/impl/ProducerServiceImpl.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import com.payments.PaymentsAPI.entity.Payment;
 import com.payments.PaymentsAPI.service.ProducerService;
 
-import com.squareup.okhttp.*;
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -12,8 +11,6 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
@@ -24,32 +21,12 @@ public final class ProducerServiceImpl implements ProducerService {
 
     private Gson gson = new Gson();
 
-    private final String webhooks_hostname = "http://localhost";
-    private URL url;
-    private OkHttpClient httpClient = new OkHttpClient();
-
     private static Properties properties() {
         var properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.102.217.31:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         return properties;
-    }
-    
-    public void sendRequest(String port, String json){
-        try {
-            RequestBody body = RequestBody.create(
-                    MediaType.parse("application/json"), json);
-
-            Request request = new Request.Builder()
-                    .url(webhooks_hostname + ":" + port + "/")
-                    .post(body)
-                    .build();
-
-            httpClient.newCall(request).execute();
-        } catch (IOException e){
-            System.out.println("Ocorreu um erro enviando uma request a um webhook");
-        }
     }
     
     @Override
--- WebhooksProcessor/src/main/java/com/processor/WebhooksProcessor/service/impl/ConsumerServiceImpl.java
+++ WebhooksProcessor/src/main/java/com/processor/WebhooksProcessor/service/impl/ConsumerServiceImpl.java
@@ -13,8 +13,18 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.stereotype.Service;
 
+import com.squareup.okhttp.*;
+
+import java.io.IOException;
+
+import com.google.gson.Gson;
+
 @Service
 public final class ConsumerServiceImpl implements ConsumerService {
+
+    private OkHttpClient httpClient = new OkHttpClient();
+    private final String webhooks_hostname = "http://localhost";
+    private Gson gson = new Gson();
 
     private static Properties properties() {
         var properties = new Properties();
@@ -24,17 +34,53 @@ public final class ConsumerServiceImpl implements ConsumerService {
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
         return properties;
     }
+
+    public void sendRequest(String port, String json){
+        try {
+            RequestBody body = RequestBody.create(
+                    MediaType.parse("application/json"), json);
+
+            Request request = new Request.Builder()
+                    .url(webhooks_hostname + ":" + port + "/")
+                    .post(body)
+                    .build();
+
+            httpClient.newCall(request).execute();
+        } catch (IOException e){
+            System.out.println("Ocorreu um erro enviando uma request a um webhook");
+        }
+    }
     
     @Override
     public void consume() {
-        var consumer = new KafkaConsumer<String, String>(properties());
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties());
         consumer.subscribe(Collections.singletonList("EXEMPLO_TOPICO"));
     
         while (true) {
-            var records = consumer.poll(Duration.ofMillis(100));
-            for (ConsumerRecord<String, String> registro : records) {
-                System.out.println(registro.key());
-                System.out.println(registro.value());
+            try {
+                ConsumerRecord<String, String> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> registro : records) {
+                    Payment pay = gson.fromJson(registro.value());
+                    switch(pay.getFlag().name()){
+                        case "MASTERCARD":
+                            sendRequest("8082", pay.getFlag().name());
+                            break;
+                        case "VISA":
+                            sendRequest("8083", pay.getFlag().name());
+                            break;
+                        case "AMERICAN_EXPRESS":
+                            sendRequest("8084", pay.getFlag().name());
+                            break;
+                        case "HIPERCARD":
+                            sendRequest("8085", pay.getFlag().name());
+                            break;
+                        case "ELO":
+                            sendRequest("8086", pay.getFlag().name());
+                            break;
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println("Erro parseando a mensagem!");
             }
         }
     }
