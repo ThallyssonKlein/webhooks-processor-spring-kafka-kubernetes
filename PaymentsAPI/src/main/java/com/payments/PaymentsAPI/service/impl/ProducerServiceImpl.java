6a78b474bc8d66caeaf297e16c5c9e5b995d1c78
thallyssonklein <thallyssonklein@gmail.com>
2021-07-15 11:31:47 -0300

    Teste2

--- PaymentsAPI/src/main/java/com/payments/PaymentsAPI/service/impl/ProducerServiceImpl.java
+++ PaymentsAPI/src/main/java/com/payments/PaymentsAPI/service/impl/ProducerServiceImpl.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.payments.PaymentsAPI.entity.Payment;
 import com.payments.PaymentsAPI.service.ProducerService;
 
+import com.squareup.okhttp.*;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
@@ -19,15 +20,18 @@ import java.net.ProtocolException;
 import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public final class ProducerServiceImpl implements ProducerService {
     
-    private Gson gson = new Gson();
     private KafkaProducer<String,String> producer = new KafkaProducer<String,String>(properties());
-    
+
+    private Gson gson = new Gson();
+
     private final String webhooks_hostname = "http://localhost";
     private URL url;
+    private OkHttpClient httpClient = new OkHttpClient();
 
     private static Properties properties() {
         var properties = new Properties();
@@ -37,30 +41,19 @@ public final class ProducerServiceImpl implements ProducerService {
         return properties;
     }
     
-    public void sendRequest(String port, String body){
-        Request request = new Request.Builder().method("POST", body)
-            .url(webhooks_hostname + ":" + port + "/")
-            .build();
-        OkHttpClient httpClient = new OkHttpClient();
-        try
-        {
-            Response response = httpClient.newBuilder()
-            .readTimeout(1, TimeUnit.SECONDS)
-            .build()
-            .newCall(request)
-            .execute();
-            if(response.isSuccessful())
-            {
-                // notification about succesful request
-            }
-            else
-            {
-                // notification about failure request
-            }
-        }
-        catch (IOException e1)
-        {
-            // notification about other problems
+    public void sendRequest(String port, String json){
+        try {
+            RequestBody body = RequestBody.create(
+                    MediaType.parse("application/json"), json);
+
+            Request request = new Request.Builder()
+                    .url(webhooks_hostname + ":" + port + "/")
+                    .post(body)
+                    .build();
+
+            httpClient.newCall(request).execute();
+        } catch (IOException e){
+            System.out.println("Ocorreu um erro enviando uma request a um webhook");
         }
     }
     
